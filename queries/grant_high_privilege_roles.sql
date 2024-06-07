60d2b620c0194a33fae5edcb6a2c185abe8bb0d6
Mitigian-Doron <104005234+Mitigian-Doron@users.noreply.github.com>
2024-06-07 17:31:22 +0300

    Delete queries/grant_high_privilege_roles.sql

--- queries/grant_high_privilege_roles.sql
+++
@@ -1,8 +0,0 @@
-SELECT *
-FROM query_history
-WHERE QUERY_TYPE = 'GRANT'
-AND (
-    QUERY_TEXT ILIKE '%grant%role%accountadmin%to%'
-    OR QUERY_TEXT ILIKE '%grant%role%securityadmin%to%'
-    OR QUERY_TEXT ILIKE '%grant%role%sysadmin%to%'
-)
