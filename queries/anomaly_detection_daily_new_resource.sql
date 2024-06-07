25b475f54832bc2bcf3163e24ec9e3fda161f7db
Mitigian-Doron <104005234+Mitigian-Doron@users.noreply.github.com>
2024-06-07 17:31:43 +0300

    Update anomaly_detection_daily_new_resource.sql

--- queries/anomaly_detection_daily_new_resource.sql
+++ queries/anomaly_detection_daily_new_resource.sql
@@ -1,15 +1,16 @@
 WITH ranked_accesses AS (
   SELECT *,
-         ROW_NUMBER() OVER(PARTITION BY USER_NAME8, DATABASE_NAME ORDER BY CAST(START_TIME AS TIMESTAMP)) AS row_number,
+         ROW_NUMBER() OVER(PARTITION BY USER_NAME,DATABASE_NAME ORDER BY CAST(START_TIME AS TIMESTAMP)) AS row_number,
          TO_DATE(CAST(START_TIME AS TIMESTAMP)) AS access_date
-  FROM query_history
+  FROM snowflake.account_usage.query_history
 ),
 first_time_accesses AS (
-  SELECT access_date, USER_NAME8, DATABASE_ID, DATABASE_NAME 
+  SELECT access_date, USER_NAME,DATABASE_NAME 
+  from ranked_accesses
   WHERE row_number = 1
 ),
 daily_new_resource_count AS (
-  SELECT access_date, USER_NAME, COUNT(*) AS count, COLLECT_SET(DATABASE_ID) AS database_ids, COLLECT_SET(DATABASE_NAME) AS database_names
+  SELECT access_date, USER_NAME, COUNT(DISTINCT DATABASE_NAME) AS count, ARRAY_AGG(DISTINCT DATABASE_NAME) AS database_names
   FROM first_time_accesses
   GROUP BY access_date, USER_NAME 
 ),
